b32b6be052448a40dd2cb3c9a758247491c46cdc
Steven-U <steven.ung@outlook.fr>
2015-04-29 14:19:49 +0200

    Create supStructures.sql

---
+++ supStructures.sql
@@ -0,0 +1,12 @@
+--Supression des données
+
+drop table AVION;
+drop table REVISION;
+drop table personnel_sol;
+drop table personnel_nav;
+drop table Client;
+drop table VOL;
+drop table Mission;
+drop table escale;
+drop table Equipage_Vol;
+drop table Reservation;
