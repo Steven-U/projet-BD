64a1c22df5fe85518499e3536d45264d075285f4
Steven-U <steven.ung@outlook.fr>
2015-05-05 17:06:02 +0200

    Update plprojet.sql

--- plprojet.sql
+++ plprojet.sql
@@ -1,16 +1,49 @@
+--TRIGGER
+
+--La capacité de ces appareils est comprise entre 15 et 30 places en fonction des types. 
+CREATE OR REPLACE TRIGGER check_place AFTER
+UPDATE OR INSERT ON AVION
+DECLARE nbplace NUMBER(2);
+BEGIN
+SELECT capacite
+INTO nbplace
+FROM AVION
+if (nbplace<15 or nbplace>30) then
+  raise_application_error ( -20002, 'La capacité doit être compris entre 15 et 30 places.');
+end;  
+
+
+--La législation aérienne impose une révision tous les 6 mois ou toutes les 500 heures
+--de vols sous peine d’interdiction de vol. 
+CREATE OR REPLACE TRIGGER check_revision AFTER
+UPDATE OR INSERT ON AVION
+DECLARE nbheure NUMBER(3);
+BEGIN
+SELECT nbheure
+INTO nbplace
+FROM AVION
+WHERE;
+end;  
+
+
+
+--Pour le personnel navigant qui compose les équipages, il est nécessaire de suivre
+--leur nombre d’heures de vol du mois en cours ainsi que la totalité du nombre d’heures de vol.
+--Le nombre d’heures maximum de vol mensuel est de 50 (temps d’escale comrpise) pour n’importe quel personnel navigant
+
+
 --Cette procédure affecte un avion à un vol donné.  
 --Définissez la signature, les contrôles fonctionnels associés
 CREATE OR REPLACE PROCEDURE P_AFFECTATION_AVION(numeroAvion NUMBER, numeroVol NUMBER) IS
 
 declare
 CURSOR c1 is select num from avion;
-v_nbh number:=0;
+
 
 BEGIN
-for enreg in c1 loop
-v_nbh := v_nbh + enreg.nbr_heure_vol;
+for  in c1 loop
 end loop;
-DBms_output.put_line('le total dheure de vol est :'||v_nbh);
+DBms_output.put_line();
 
 end
 ;
@@ -47,8 +80,13 @@ select ;
 
 BEGIN
 
-open c1
-fetch 
+open c1;
+loop
+  fetch c1 into 
+  exit when c1%notfound;
+end loop
+close c1;
+
 
 
 DBms_output.put_line('résultat ok');
@@ -63,17 +101,17 @@ CREATE OR REPLACE PROCEDURE P_HIST_RESERVATION(dateHisto DATE)
 
 DECLARE
 
-//
-
 CURSOR c1 IS
 select ;
 
 BEGIN
 
-open c1
-fetch 
-
-close c1
+open c1;
+loop
+  fetch c1 into 
+  exit when c1%notfound;
+end loop
+close c1;
 
 CREATE TABLE Hist_Reservation(dateHisto Date);
 
