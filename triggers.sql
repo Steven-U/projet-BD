16ae3733bb67e5a4b2b5c0a19e3782f0da8d7576
Steven-U <steven.ung@outlook.fr>
2015-05-08 01:48:17 +0200

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,20 +1,16 @@
 -- Trigger qui vérifie que l'heure de départ < l'heure d'arrivée
-CREATE TRIGGER T1 ON VOL
-FOR INSERT
-AS
+CREATE OR REPLACE TRIGGER check_place AFTER
+UPDATE OR INSERT ON AVION
+DECLARE 
+    nbplace NUMBER(2);
 BEGIN
-DECLARE @v_depart varchar(15)
-DECLARE @v_arrivée varchar(15)
-DECLARE @h_depart datetime
-DECLARE @h_arrivée %type
-SELECT @ville_dep=AeroportDepart, @ville_dep=AeroportArrivée, @h_Depart=h_dep, @h_arrivée=h_arr
-FROM INSERTED
-IF @ville_dep = @ville_arr OR @h_dep>@h_arr
-BEGIN
-RAISERROR(‘Insertion impossible !!’, 16, 1)
-ROLLBACK
-END
-END 
+    SELECT capacite INTO nbplace
+    FROM AVION;
+    
+    if (nbplace<15 or nbplace>30) then
+      raise_application_error ( -20002, 'La capacité doit être compris entre 15 et 30 places.');
+    endif;
+end; 
 
 --TRIGGER
 
