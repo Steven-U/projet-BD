69fac7dc77cc6a6fa5326fb772f71207a3e95126
Steven-U <steven.ung@outlook.fr>
2015-05-08 22:26:34 +0200

    Update req.sql

--- req.sql
+++ req.sql
@@ -1,15 +1,3 @@
-/*
-TABLE AVION
-TABLE REVISION
-TABLE PERSONNEL_SOL
-TABLE PERSONNEL_NAV
-TABLE MISSION
-TABLE VOL
-TABLE CLIENT
-TABLE Reservation
-TABLE EQUIPAGE_VOL
-*/
-
 --REQUETE SQL
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -67,14 +55,14 @@ and date_rev = (Select max(date_rev) from revision);
 
 	WITH TabCount AS
 	(
-	Select m.num, COUNT(*) AS N,
-		   MAX(count(*)) OVER() AS maxi -- Over retourne toutes les valeurs max
-	FROM   MISSION m, ESCALE e
-	Where e.num_mission = m.num
-	GROUP BY m.num)
+	Select mis.num, COUNT(*) AS N,
+		   MAX(count(*)) OVER() AS maximum -- Over retourne toutes les valeurs max
+	FROM   MISSION mis, ESCALE e
+	Where e.num_mission = mis.num
+	GROUP BY mis.num)
 	SELECT TabCount.num as Num_Mission, N as NB_escale
 	FROM   TabCount
-	WHERE  maxi = N;
+	WHERE  maximum = N;
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------
 -- 6 ° ) Quelle est la ville comportant le plus de mission de vols au départ et Ã  l'arrivée confondus ?
@@ -97,44 +85,18 @@ and date_rev = (Select max(date_rev) from revision);
 -- 7 ° ) Quel est le nom du pilote affecté au vol du 19 décembre 2014 au départ de Créteil Ã  10h00 ?
 --------------------------------------------------------------------------------------------------------------------------------------------------------
  
- drop table equipage_vol;
- drop table vol cascade constraints;
- 
-CREATE TABLE VOL (
-	id 				NUMBER NOT NULL,
-	date_depart 	DATE NOT NULL,
-	avion 			NUMBER NOT NULL,
-	ville_dep 		varchar2(50) NOT NULL,
-	ville_arr 		varchar2(50) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (avion) REFERENCES avion(id)
-); 
-
-CREATE TABLE Equipage_Vol (
-	id_vol 			NUMBER NOT NULL,
-	id_personnel 	NUMBER NOT NULL,
-	PRIMARY KEY (id_vol, id_personnel),
-	FOREIGN KEY (id_vol) REFERENCES vol(id) ON DELETE CASCADE, 
-	FOREIGN KEY (id_personnel) REFERENCES personnel_nav(id) ON DELETE CASCADE
-);
- 
 --Test
 --Créteil à Annecy
 
-INSERT INTO VOL VALUES('1','19/12/2014','1','Créteil','Annecy');
-INSERT INTO VOL VALUES('2','08/05/2015','1','Créteil','Annecy');
-
-insert into equipage_vol (id_vol,id_personnel) values(1,1);
-
-	Select pn.nom, pn.prenom
-	from Equipage_VOL ev, 
+	Select pers_nav.nom, pers_nav.prenom
+	from Equipage_VOL equip_v, 
         vol v, 
         mission mis,
-        personnel_nav pn
-	where ev.id_vol = v.id
-	and mis.num_vol = v.id
-	and ev.id_personnel = pn.id
-	and pn.fonction  = 'pilote'
+        personnel_nav pers_nav
+	where equip_v.id_vol = v.id
+	and mis.num = v.id_mission
+	and equip_v.id_personnel = pers_nav.id
+	and pers_nav.fonction  = 'pilote'
 	and v.date_depart = '19/12/2014'
 	and mis.ville_dep like 'Créteil'
 	and mis.h_dep = 10;
@@ -156,12 +118,7 @@ insert into equipage_vol (id_vol,id_personnel) values(1,1);
 --------------------------------------------------------------------------------------------------------------------------------------------------------
 -- 9 ° ) Quels sont les pilotes qui ne sont jamais allés Ã  Annecy ?
 --------------------------------------------------------------------------------------------------------------------------------------------------------
-
-INSERT INTO VOL VALUES(seqvol.nextval,'2','08/05/2015','2','Créteil','Brest');
-INSERT INTO VOL VALUES(seqvol.nextval,'2','19/12/2014','1','Créteil','Annecy');
-INSERT INTO equipage_vol (id_vol,id_personnel) values(2,2);
-
---MARCHE A PRIORI A TESTER 
+ 
   	SELECT distinct
 		pn.nom, pn.prenom,
 		M.num,
@@ -174,8 +131,9 @@ INSERT INTO equipage_vol (id_vol,id_personnel) values(2,2);
     and ev.id_vol = v.id
 		and pn.fonction  = 'pilote'
 		and M.ville_arr != 'Annecy'
-		and E.num_mission = M.num (+);
-	--	and E.ville != 'Annecy';
+    and M.ville_dep != 'Annecy'
+		and E.num_mission = M.num (+)
+		and E.ville != 'Annecy';
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------	
 -- 10 ° ) Quels sont les vols au départ de Créteil ayant une escale maximum ?
@@ -196,20 +154,23 @@ INSERT INTO equipage_vol (id_vol,id_personnel) values(2,2);
 -- 11 ° ) Quels sont les vols ayant été effectués sans escale ?
 --------------------------------------------------------------------------------------------------------------------------------------------------------
 
---MARCHE A PRIORI A TESTER 
+INSERT INTO mission(num, date_depart ,h_dep,ville_dep,ville_arr) values ('9','20/06/2015','10', 'Créteil','Marseille');
+INSERT INTO VOL (id,date_depart,avion,id_mission) VALUES(seqvol.nextval,'08/05/2015','2','9');
 
-	Select id
+	Select id as vol
 	from vol
-	where num_mission not in (Select num_mission from escale);
+	where id_mission not in (Select id_mission from escale);
   
 --------------------------------------------------------------------------------------------------------------------------------------------------------
 -- 12 ° ) Quels sont les avions actuellement en vol Ã  ce jour et cette heure ?
 --------------------------------------------------------------------------------------------------------------------------------------------------------
---MARCHE
+
+insert into mission values('10','08/05/2015',22,'Créteil','Paris');
+insert into vol values (seqvol.nextval,'08/05/2015','2','10');
 
 	Select type_avion
 	from AVION a, vol v, mission m, escale e
-	where m.num = v.num_mission
+	where m.num = v.id_mission
 	and e.num_mission = m.num
 	and a.id = v.avion
 	and M.date_depart = to_char(sysdate,'DD/MM/YYYY')
@@ -224,7 +185,7 @@ INSERT INTO equipage_vol (id_vol,id_personnel) values(2,2);
 	from client c, Reservation r, mission m, vol v
 	where c.id = r.id_client
 	and r.id_vol = m.num
-	and v.num_mission = m.num
+	and v.id_mission = m.num
 	and m.date_depart = '03/06/2014'
 	and m.ville_dep = 'Enghien'
 	and m.ville_arr = 'Annecy';
