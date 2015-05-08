87ef014300b7e4af0dc46450299188ea7aa34490
Steven-U <steven.ung@outlook.fr>
2015-05-08 17:14:42 +0200

    Update sequence.sql

--- sequence.sql
+++ sequence.sql
@@ -1,102 +1,62 @@
 --Séquence pour l'auto-incrémenation(id) de la table AVION
 
 CREATE SEQUENCE seqavion
-
   nomaxvalue
-
   start with 1
-
   increment by 1 ;
-
-
 
 --Séquence pour l'auto-incrémenation(id) de la table CLIENT
 
 CREATE SEQUENCE seqclient
-
   nomaxvalue
-
   start with 1
-
   increment by 1 ;
-
-
 
 --Séquence pour l'auto-incrémenation(num) de la table ESCALE
 
 CREATE SEQUENCE seqescale
-
   nomaxvalue
-
   start with 1
-
   increment by 1 ;
-
 
 --Séquence pour l'auto-incrémenation(num) de la table MISSION
 
 CREATE SEQUENCE seqmission
-
   nomaxvalue
-
   start with 1
-
   increment by 1 ;
-
 
 --Séquence pour l'auto-incrémenation(id) de la table PERSONNEL_NAV
 
 CREATE SEQUENCE seqpersnav
-
   nomaxvalue
-
   start with 1
-
   increment by 1 ;
-
-
 
 --Séquence pour l'auto-incrémenation(id) de la table PERSONNEL_SOL
 
 CREATE SEQUENCE seqperssol
-
   nomaxvalue
-
   start with 1
-
   increment by 1 ;
-
-
 
 --Séquence pour l'auto-incrémenation(id) de la table RESERVATION
 
 CREATE SEQUENCE seqreservation
-
   nomaxvalue
-
   start with 1
-
   increment by 1 ;
-
-
 
 --Séquence pour l'auto-incrémenation(id) de la table REVISION
 
 CREATE SEQUENCE seqrevision
-
   start with 1
-
   increment by 1
-
   nomaxvalue;
-
-
 
 --Séquence pour l'auto-incrémenation(id) de la table VOL
 
 CREATE SEQUENCE seqvol
-
   nomaxvalue
-
   start with 1
   increment by 1 ;
